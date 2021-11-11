6cf1dd13c6df8ea73b6c83bdb6608660c6012a61
Erik Mende <emende@Eriks-MacBook-Pro.local>
2021-11-11 15:28:20 +0200

    added header file

---
+++ libft.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: emende <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/11 13:59:36 by emende            #+#    #+#             */
+/*   Updated: 2021/11/11 15:28:05 by emende           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <stdlib.h>
+
+size_t	ft_strlen(const char *s);
+char	*ft_strcpy(char *dst, const char *src);
+char	*ft_strdup(const char *s1);
+
+#endif
