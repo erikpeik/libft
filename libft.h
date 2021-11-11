fed4bded8d7916a04c27ba15f6e15761791a16b2
Erik Mende <erikpeik@outlook.com>
2021-11-11 18:38:10 +0200

    added new function strncmp()

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: emende <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/11 13:59:36 by emende            #+#    #+#             */
-/*   Updated: 2021/11/11 18:00:39 by emende           ###   ########.fr       */
+/*   Updated: 2021/11/11 18:26:10 by emende           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@
 size_t	ft_strlen(const char *s);
 char	*ft_strcpy(char *dst, const char *src);
 char	*ft_strdup(const char *s1);
+char	*ft_strncpy(char *dst, const char *src, size_t len);
 int		ft_strcmp(const char *s1, const char *s2);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
 
 #endif
