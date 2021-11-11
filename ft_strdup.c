9deeeb23f59574b63d960bfb240f6a1636568d81
Erik Mende <emende@c2r1p1.hive.fi>
2021-11-11 16:32:49 +0200

    ft_strdup maybe working now

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: emende <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/11 13:19:06 by emende            #+#    #+#             */
-/*   Updated: 2021/11/11 14:04:39 by emende           ###   ########.fr       */
+/*   Updated: 2021/11/11 16:10:31 by emende           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,20 @@
 
 char	*ft_strdup(const char *s1)
 {
-	char *copy;
+	char	*copy;
+	int		i;
 
+	i = 0;
 	copy = (char *)malloc(sizeof(copy) * ft_strlen(s1));
+	if (!copy)
+	{
+		return (NULL);
+	}
+	while (s1[i])
+	{	
+		copy[i] = s1[i];
+		i++;
+	}
+	copy[i] = s1[i];
+	return (copy);
 }
