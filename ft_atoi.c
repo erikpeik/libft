f4babee5270837b393bce9d1007b15605cb36470
Erik Mende <erikpeik@outlook.com>
2021-11-12 19:43:00 +0200

    ft_atoi.c done

---
+++ ft_atoi.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: emende <emende@student.hive.fi>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/12 17:35:53 by emende            #+#    #+#             */
+/*   Updated: 2021/11/12 19:41:32 by emende           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdio.h>
+
+int	ft_atoi(const char *str)
+{
+	int	res;
+	int	i;
+	int	sign;
+
+	sign = 1;
+	res = 0;
+	i = 0;
+	while (str[i] == '\t' || str[i] == '\n' || str[i] == '\v' || \
+			str[i] == '\f' || str[i] == ' ' || str[i] == '\r')
+		i++;
+	if (str[i] == '-')
+	{
+		sign *= -1;
+		i++;
+	}
+	else if (str[i] == '+')
+		i++;
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		res = res * 10 + str[i] - '0';
+		i++;
+	}
+	return (sign * res);
+}
