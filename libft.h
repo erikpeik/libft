5416f38ec9242184c68751a74d9ba894fe1ade20
Erik Mende <emende@Eriks-MacBook-Pro.local>
2021-11-17 19:27:29 +0200

    added function ft_strchr() that locate character in string

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: emende <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/11 13:59:36 by emende            #+#    #+#             */
-/*   Updated: 2021/11/16 17:52:05 by emende           ###   ########.fr       */
+/*   Updated: 2021/11/17 15:38:21 by emende           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,7 @@ size_t	ft_strlen(const char *s);
 char	*ft_strcpy(char *dst, const char *src);
 char	*ft_strdup(const char *s1);
 char	*ft_strncpy(char *dst, const char *src, size_t len);
+char	*ft_strchr(const char *s, int c);
 int		ft_strcmp(const char *s1, const char *s2);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 int		ft_atoi(const char *str);
