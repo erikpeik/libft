a3141c854c5963dae7d89d011d85b0a9a1dd53fd
Erik Mende <erikpeik@outlook.com>
2021-11-24 23:15:09 +0200

    added ft_strstr() function prototype and categories for every return type

--- libft.h
+++ libft.h
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   libft.h                                      :+:        +:+:+: :+:+:+    */
 /*                                                    +:+ +:+         +:+     */
 /*   By: emende <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/11 13:59:36 by emende            #+#    #+#             */
-/*   Updated: 2021/11/24 17:35:21 by emende           ###   ########.fr       */
+/*   Updated: 2021/11/24 23:13:07 by emende       ########## ###       ###    */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,11 @@
 # include <stdlib.h>
 # include <unistd.h>
 
+/* Part 1 - Libc function */
+
 size_t	ft_strlen(const char *s);
 size_t	ft_strlcat(char	*dst, const char *src, size_t dstsize);
+
 char	*ft_strcpy(char *dst, const char *src);
 char	*ft_strdup(const char *s1);
 char	*ft_strncpy(char *dst, const char *src, size_t len);
@@ -25,6 +28,8 @@ char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
 char	*ft_strcat(char	*s1, const char *s2);
 char	*ft_strncat(char *s1, const char *s2, size_t n);
+char	*ft_strstr(const char *haystack, const char *needle);
+
 int		ft_strcmp(const char *s1, const char *s2);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 int		ft_atoi(const char *str);
@@ -35,8 +40,11 @@ int		ft_isalnum(int c);
 int		ft_isprint(int c);
 int		ft_toupper(int c);
 int		ft_tolower(int c);
+
 void	*ft_memset(void *b, int c, size_t len);
 void	ft_bzero(void *s, size_t n);
+
+/* Part 2 - Additional function */
 
 void	ft_putchar(char c);
 void	ft_putstr(char const *s);
