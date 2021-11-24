72287f3dc0e13e6ee6165a4b5f79c32b6bda27ce
Erik Mende <erikpeik@outlook.com>
2021-11-24 22:44:31 +0200

    ft_strlcat() maybe functional

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*   ft_strlcat.c                                 :+:        +:+:+: :+:+:+    */
 /*                                                    +:+ +:+         +:+     */
 /*   By: emende <emende@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/24 17:23:14 by emende            #+#    #+#             */
-/*   Updated: 2021/11/24 17:49:42 by emende           ###   ########.fr       */
+/*   Updated: 2021/11/24 22:09:01 by emende       ########## ###       ###    */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,21 @@
 
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
-	
+	size_t	i;
+	size_t	z;
+	char	*buffer;
+	buffer = ft_strdup(dst);
+
+	i = ft_strlen(dst);
+	z = 0;
+	if (dstsize <= 0 || ft_strlen(dst) >= dstsize)
+		return (i);
+	while (z < ft_strlen(src) && i < dstsize - 1)
+	{
+		dst[i] = src[z];
+		z++;
+		i++;
+	}
+	dst[i] = '\0';
+	return (ft_strlen(buffer) + ft_strlen(src));
 }
