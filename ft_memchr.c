5a89ff335acd9057ae79de397726ed1a69840f5f
Erik Mende <erikpeik@outlook.com>
2021-11-27 00:20:59 +0200

    Fixed norminatte error in ft_memchr()

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: emende <emende@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/26 20:07:23 by emende            #+#    #+#             */
-/*   Updated: 2021/11/26 21:12:07 by emende           ###   ########.fr       */
+/*   Updated: 2021/11/27 00:20:29 by emende           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	*ft_memchr(const void *s, int c, size_t n)
 	size_t	i;
 
 	i = 0;
-	while(i < n && ((unsigned char *)s))
+	while (i < n && ((unsigned char *)s))
 	{
 		if (*(unsigned char *)s == (unsigned char )c)
 			return ((void *)s);
