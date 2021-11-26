9df4864a1f40907b2a051188c09a214c8363f24d
Erik Mende <erikpeik@outlook.com>
2021-11-27 00:20:04 +0200

    one line was too long

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: emende <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/17 22:21:03 by emende            #+#    #+#             */
-/*   Updated: 2021/11/26 19:44:27 by emende           ###   ########.fr       */
+/*   Updated: 2021/11/27 00:18:35 by emende           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,10 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 	size_t	i;
 
 	i = 0;
-
 	if (ft_strlen(needle) == 0)
 		return ((char *)haystack);
-	while ((char )haystack[i] && ((int )i <= ((int )len - (int )ft_strlen(needle))))
+	while ((char )haystack[i] && \
+			(int )i <= ((int )(len - ft_strlen(needle))))
 	{
 		if (ft_strncmp(haystack + i, needle, ft_strlen(needle)) == 0)
 			return ((char *)haystack + i);
