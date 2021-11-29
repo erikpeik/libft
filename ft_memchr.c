f69bcc92b67019a3c206e81244396c3d1b728b8d
Erik Mende <erikpeik@outlook.com>
2021-11-29 19:19:39 +0200

    Added if statement that checks if sring is null

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: emende <emende@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/26 20:07:23 by emende            #+#    #+#             */
-/*   Updated: 2021/11/27 00:20:29 by emende           ###   ########.fr       */
+/*   Updated: 2021/11/29 18:49:43 by emende           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,11 @@ void	*ft_memchr(const void *s, int c, size_t n)
 	size_t	i;
 
 	i = 0;
-	while (i < n && ((unsigned char *)s))
+	if (!(const unsigned char *)s)
+		return (NULL);
+	while (i < n)
 	{
-		if (*(unsigned char *)s == (unsigned char )c)
+		if (*(const unsigned char *)s == (unsigned char )c)
 			return ((void *)s);
 		s++;
 		i++;
