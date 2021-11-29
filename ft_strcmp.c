b00cc3e22abfa8af3dcb0d1cb419c3342f1ef6e6
Erik Mende <erikpeik@outlook.com>
2021-11-30 00:29:52 +0200

    removed brackets

--- ft_strcmp.c
+++ ft_strcmp.c
@@ -6,7 +6,7 @@
 /*   By: emende <emende@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/11 17:48:35 by emende            #+#    #+#             */
-/*   Updated: 2021/11/30 00:21:42 by emende           ###   ########.fr       */
+/*   Updated: 2021/11/30 00:28:20 by emende           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,7 @@ int	ft_strcmp(const char *s1, const char *s2)
 	while (s1[i] || s2[i])
 	{
 		if (s1[i] != s2[i])
-		{
 			return (((unsigned char *)s1)[i] - ((unsigned char *)s2)[i]);
-		}
 		i++;
 	}
 	return (0);
