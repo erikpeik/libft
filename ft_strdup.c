1960e7b1626ea3117b6aec763b603d584dd886fa
Erik Mende <erikpeik@outlook.com>
2021-11-29 23:07:16 +0200

    ft_strdup() now mallocing right ammount memory

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: emende <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/11 13:19:06 by emende            #+#    #+#             */
-/*   Updated: 2021/11/11 16:10:31 by emende           ###   ########.fr       */
+/*   Updated: 2021/11/29 23:05:07 by emende           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,17 @@
 char	*ft_strdup(const char *s1)
 {
 	char	*copy;
-	int		i;
+	size_t	i;
 
 	i = 0;
-	copy = (char *)malloc(sizeof(copy) * ft_strlen(s1));
+	copy = (char *)malloc(sizeof(char) * (ft_strlen(s1) + 1));
 	if (!copy)
-	{
 		return (NULL);
-	}
 	while (s1[i])
-	{	
+	{
 		copy[i] = s1[i];
 		i++;
 	}
-	copy[i] = s1[i];
+	copy[i] = '\0';
 	return (copy);
 }
