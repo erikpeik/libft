8806c3709ddfde4f05660b28cea547648f274c0c
Erik Mende <erikpeik@outlook.com>
2021-11-29 17:32:45 +0200

    Now hopefully ft_memcpy() is working

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: emende <emende@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/26 22:50:54 by emende            #+#    #+#             */
-/*   Updated: 2021/11/26 23:20:21 by emende           ###   ########.fr       */
+/*   Updated: 2021/11/29 17:31:26 by emende           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,11 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 	size_t	i;
 
 	i = 0;
+	if (!dst && !src)
+		return (NULL);
 	while (i < n)
 	{
-		*(unsigned char *)dst = *(unsigned char *)src;
-		dst++;
-		src++;
+		((unsigned char *)dst)[i] = ((unsigned char *)src)[i];
 		i++;
 	}
 	return ((void *)dst);
