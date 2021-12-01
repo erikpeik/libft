cefa5a65a6cacd538aa9f803c181c59a5bc02ec3
Erik Mende <erikpeik@outlook.com>
2021-12-01 21:04:11 +0200

    Added libft.h include in ft_toupper function

--- ft_toupper.c
+++ ft_toupper.c
@@ -6,9 +6,11 @@
 /*   By: emende <emende@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/14 18:16:46 by emende            #+#    #+#             */
-/*   Updated: 2021/11/14 18:31:05 by emende           ###   ########.fr       */
+/*   Updated: 2021/12/01 20:59:11 by emende           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "libft.h"
 
 int	ft_toupper(int c)
 {
