04cc0ee94effe24c670dc136af748625256e06c6
Erik Mende <erikpeik@outlook.com>
2021-12-01 17:30:24 +0200

    if len was 0, there was small problem with size_t. fixed now.

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: emende <emende@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/27 19:34:32 by emende            #+#    #+#             */
-/*   Updated: 2021/11/27 21:30:43 by emende           ###   ########.fr       */
+/*   Updated: 2021/12/01 17:26:44 by emende           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,23 +16,29 @@ void	*ft_memmove(void *dst, const void *src, size_t len)
 {
 	size_t	i;
 
+	if (!dst && !src)
+		return (NULL);
 	if (dst < src)
 	{
 		i = 0;
 		while (i < len)
 		{
-			((char *)dst)[i] = ((char *)src)[i];
+			((char *) dst)[i] = ((char *) src)[i];
 			i++;
 		}
 	}
 	else
 	{
-		i = len - 1;
-		while (i >= 0)
+		if (len > 0)
+			i = len - 1;
+		else
+			return (dst);
+		while (i > 0)
 		{
-			((char *)dst)[i] = ((char *)src)[i];
+			((char *) dst)[i] = ((char *) src)[i];
 			i--;
 		}
+		((char *) dst)[i] = ((char *) src)[i];
 	}
-	return ((void *)dst);
+		return (dst);
 }
