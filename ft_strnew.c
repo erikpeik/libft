10dbf937bdcff167de713e3714d68d5f3d5589d0
Erik Mende <erikpeik@outlook.com>
2021-12-03 20:08:27 +0200

    fixed norm error

--- ft_strnew.c
+++ ft_strnew.c
@@ -6,7 +6,7 @@
 /*   By: emende <emende@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/03 19:27:28 by emende            #+#    #+#             */
-/*   Updated: 2021/12/03 19:50:30 by emende           ###   ########.fr       */
+/*   Updated: 2021/12/03 20:08:04 by emende           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ char	*ft_strnew(size_t size)
 
 	str = (unsigned char *) malloc(sizeof(*str) * (size + 1));
 	if (!str)
-		return((char *) NULL);
+		return ((char *) NULL);
 	ft_bzero(str, (size + 1));
 	return ((char *) str);
 }
