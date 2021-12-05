0f6600a5212925bf7de37564012024899df9ec4f
Erik Mende <erikpeik@outlook.com>
2021-12-05 17:18:22 +0200

    ft_intlen -- find length of digits in integer

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: emende <emende@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/28 16:00:48 by emende            #+#    #+#             */
-/*   Updated: 2021/12/05 16:15:06 by emende           ###   ########.fr       */
+/*   Updated: 2021/12/05 17:08:18 by emende           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -86,5 +86,6 @@ int		ft_strnequ(char const *s1, char const *s2, size_t n);
 /* Selfmade extra functions */
 
 size_t	ft_strclen(char const *s, char c);
+size_t	ft_intlen(int n);
 
 #endif
