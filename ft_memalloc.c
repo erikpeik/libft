c4a0a2cb49dded7d282af5a1c966467577a028f2
Erik Mende <erikpeik@outlook.com>
2021-12-07 21:57:08 +0200

    Had wrong sizeof before, now it's sizeof(*new)

--- ft_memalloc.c
+++ ft_memalloc.c
@@ -6,7 +6,7 @@
 /*   By: emende <emende@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/03 18:37:40 by emende            #+#    #+#             */
-/*   Updated: 2021/12/03 18:47:15 by emende           ###   ########.fr       */
+/*   Updated: 2021/12/07 20:10:10 by emende           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void	*ft_memalloc(size_t size)
 {
 	void	*new;
 
-	new = malloc(sizeof(new) * size);
+	new = malloc(sizeof(*new) * size);
 	if (!new)
 		return ((void *) NULL);
 	ft_memset(new, 0, size);
