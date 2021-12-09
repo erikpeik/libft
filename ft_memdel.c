c6a7eba31d8ec9e2f5da5ddb25605b4959cf6be5
Erik Mende <emende@student.hive.fi>
2021-12-09 21:34:12 +0200

    secured second pointer

--- ft_memdel.c
+++ ft_memdel.c
@@ -6,7 +6,7 @@
 /*   By: emende <emende@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/03 18:48:06 by emende            #+#    #+#             */
-/*   Updated: 2021/12/03 19:13:05 by emende           ###   ########.fr       */
+/*   Updated: 2021/12/09 20:03:42 by emende           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	ft_memdel(void **ap)
 {
-	if (ap)
+	if (ap && *ap)
 	{
 		free(*ap);
 		*ap = NULL;
