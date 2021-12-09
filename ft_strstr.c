33c232b48bd032af1d7d4adf546705db3d0b8b80
Erik Mende <emende@student.hive.fi>
2021-12-09 19:39:42 +0200

    unnecessary cast in while loop

--- ft_strstr.c
+++ ft_strstr.c
@@ -6,7 +6,7 @@
 /*   By: emende <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/17 22:21:03 by emende            #+#    #+#             */
-/*   Updated: 2021/11/26 10:28:28 by emende           ###   ########.fr       */
+/*   Updated: 2021/12/09 19:26:22 by emende           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ char	*ft_strstr(const char *haystack, const char *needle)
 	i = 0;
 	if (ft_strlen(needle) == 0)
 		return ((char *)haystack);
-	while ((char )haystack[i])
+	while (haystack[i])
 	{
 		if (ft_strncmp(haystack + i, needle, ft_strlen(needle)) == 0)
 			return ((char *)haystack + i);
