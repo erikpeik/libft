5141cc37de0cd1565cc7f44797bf4da778d7b93b
Erik Mende <emende@student.hive.fi>
2021-12-09 19:36:41 +0200

    Removed unnecessary cast

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: emende <emende@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/26 22:50:54 by emende            #+#    #+#             */
-/*   Updated: 2021/11/29 17:31:26 by emende           ###   ########.fr       */
+/*   Updated: 2021/12/09 19:14:49 by emende           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,5 +24,5 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 		((unsigned char *)dst)[i] = ((unsigned char *)src)[i];
 		i++;
 	}
-	return ((void *)dst);
+	return (dst);
 }
