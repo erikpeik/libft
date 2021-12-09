bf58ad74180e2d97e517e1fd382e57eebfa5034b
Erik Mende <emende@student.hive.fi>
2021-12-09 19:41:18 +0200

    Saved needle len not calling it thausand times

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: emende <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/17 22:21:03 by emende            #+#    #+#             */
-/*   Updated: 2021/11/27 00:18:35 by emende           ###   ########.fr       */
+/*   Updated: 2021/12/09 19:32:19 by emende           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,16 @@
 
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
-	size_t	i;
+	int		i;
+	size_t	needlelen;
 
-	i = 0;
-	if (ft_strlen(needle) == 0)
+	needlelen = ft_strlen(needle);
+	if (needlelen == 0)
 		return ((char *)haystack);
-	while ((char )haystack[i] && \
-			(int )i <= ((int )(len - ft_strlen(needle))))
+	i = 0;
+	while (haystack[i] && i <= ((int )(len - needlelen)))
 	{
-		if (ft_strncmp(haystack + i, needle, ft_strlen(needle)) == 0)
+		if (ft_strncmp(haystack + i, needle, needlelen) == 0)
 			return ((char *)haystack + i);
 		i++;
 	}
