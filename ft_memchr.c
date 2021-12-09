b2c4f6e21bd2d0291dd3471400884beb658e8aef
Erik Mende <emende@student.hive.fi>
2021-12-09 19:35:32 +0200

    removed temp files to secure the const void

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: emende <emende@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/26 20:07:23 by emende            #+#    #+#             */
-/*   Updated: 2021/11/29 23:19:39 by emende           ###   ########.fr       */
+/*   Updated: 2021/12/09 19:18:59 by emende           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,13 @@
 void	*ft_memchr(const void *s, int c, size_t n)
 {
 	size_t			i;
-	unsigned char	*str;
-	unsigned char	cr;
 
 	i = 0;
-	str = (unsigned char *)s;
-	cr = (unsigned char )c;
 	while (i < n)
 	{
-		if (*str == cr)
-			return ((void *)str);
-		str++;
+		if (*((unsigned char *)s) == ((unsigned char)c))
+			return ((void *)s);
+		s++;
 		i++;
 	}
 	return (NULL);
