b50c8aa79cbc0a97f729b42c72de2cb6d19ec698
Erik Mende <emende@student.hive.fi>
2021-12-09 21:40:29 +0200

    secured that functions isn't NULL

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -6,7 +6,7 @@
 /*   By: emende <emende@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/03 22:24:10 by emende            #+#    #+#             */
-/*   Updated: 2021/12/03 22:35:33 by emende           ###   ########.fr       */
+/*   Updated: 2021/12/09 20:06:52 by emende           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,11 @@ char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 	unsigned int	i;
 	char			*str;
 
-	if (!s)
-		return ((char *) NULL);
+	if (!s || !f)
+		return (NULL);
 	str = malloc(sizeof(*str) * (ft_strlen(s) + 1));
 	if (!str)
-		return ((char *) NULL);
+		return (NULL);
 	i = 0;
 	while (s[i])
 	{
