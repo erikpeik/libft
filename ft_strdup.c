6127a2e1f8349451e1a06867b7a752bf3e21d6b6
Erik Mende <emende@student.hive.fi>
2021-12-09 19:38:49 +0200

    Changed position where i call 'i = 0
    '

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: emende <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/11 13:19:06 by emende            #+#    #+#             */
-/*   Updated: 2021/11/29 23:05:07 by emende           ###   ########.fr       */
+/*   Updated: 2021/12/09 19:20:31 by emende           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,10 @@ char	*ft_strdup(const char *s1)
 	char	*copy;
 	size_t	i;
 
-	i = 0;
 	copy = (char *)malloc(sizeof(char) * (ft_strlen(s1) + 1));
 	if (!copy)
 		return (NULL);
+	i = 0;
 	while (s1[i])
 	{
 		copy[i] = s1[i];
