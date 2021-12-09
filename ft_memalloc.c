a0a9a5338ef55eefc6f99a6e70c83408b72afbcc
Erik Mende <emende@student.hive.fi>
2021-12-09 21:32:16 +0200

    changed return unnecessary

--- ft_memalloc.c
+++ ft_memalloc.c
@@ -6,7 +6,7 @@
 /*   By: emende <emende@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/03 18:37:40 by emende            #+#    #+#             */
-/*   Updated: 2021/12/07 20:10:10 by emende           ###   ########.fr       */
+/*   Updated: 2021/12/09 20:03:26 by emende           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ void	*ft_memalloc(size_t size)
 
 	new = malloc(sizeof(*new) * size);
 	if (!new)
-		return ((void *) NULL);
+		return (NULL);
 	ft_memset(new, 0, size);
-	return ((void *) new);
+	return (new);
 }
