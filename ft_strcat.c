560ab77458e9138dbfaf00225afef333bce67a6a
Erik Mende <emende@student.hive.fi>
2021-12-09 19:37:25 +0200

    not calling ft_strlen inside while loop

--- ft_strcat.c
+++ ft_strcat.c
@@ -6,7 +6,7 @@
 /*   By: emende <emende@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/24 15:59:40 by emende            #+#    #+#             */
-/*   Updated: 2021/11/24 16:42:20 by emende           ###   ########.fr       */
+/*   Updated: 2021/12/09 19:23:02 by emende           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,12 @@ char	*ft_strcat(char *s1, const char *s2)
 {
 	size_t	i;
 	size_t	z;
+	size_t	len;
 
 	z = 0;
 	i = ft_strlen(s1);
-	while (z < ft_strlen(s2))
+	len = ft_strlen(s2);
+	while (z < len)
 	{
 		s1[i] = s2[z];
 		z++;
