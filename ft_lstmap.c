b9e4fcad4fe25f57506750d59b2bfb23925e90d5
Erik Mende <erikpeik@outlook.com>
2021-12-12 23:37:47 +0200

    3 last bonus functions added

---
+++ ft_lstmap.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstmap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: emende <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/12 22:44:44 by emende            #+#    #+#             */
+/*   Updated: 2021/12/12 23:14:17 by emende           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem))
+{
+	t_list	*new;
+	t_list	*start;
+
+	if (!lst || !f || !(*f))
+		return (NULL);
+	new = f(lst);
+	if (!new)
+	{
+		free(new);
+		return (NULL);
+	}
+	start = new;
+	while (lst->next)
+	{
+		lst = lst->next;
+		new->next = f(lst);
+		if (!(new->next))
+		{
+			free(new->next);
+			return (NULL);
+		}
+		new = new->next;
+	}
+	return (start);
+}
