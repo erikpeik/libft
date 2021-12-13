7a4fc8a2fd7bfe5cb850b7e2127a01d58a9e16e2
Erik Mende <emende@student.hive.fi>
2021-12-13 17:12:09 +0200

    Had one small mistake, if the str was f then it would return .

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: emende <emende@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/04 00:16:15 by emende            #+#    #+#             */
-/*   Updated: 2021/12/04 16:24:17 by emende           ###   ########.fr       */
+/*   Updated: 2021/12/13 17:11:16 by emende           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,7 +46,7 @@ char	*ft_strtrim(char const *s)
 		return (NULL);
 	start = ft_get_start(s);
 	end = ft_get_end(s);
-	if (start >= end)
+	if (start > end)
 		return (ft_strdup(""));
 	trim = (char *)malloc(sizeof(char) * (size_t)(end - start + 2));
 	if (!trim)
