963e263da1ffb81b40d6ee2ebf0a8e4119c81219
Erik Mende <emende@student.hive.fi>
2021-12-14 18:25:32 +0200

    Changed size_t -> int

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: emende <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/17 22:21:03 by emende            #+#    #+#             */
-/*   Updated: 2021/12/09 19:32:19 by emende           ###   ########.fr       */
+/*   Updated: 2021/12/14 18:21:44 by emende           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,14 @@
 
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
-	int		i;
-	size_t	needlelen;
+	int	i;
+	int	needlelen;
 
-	needlelen = ft_strlen(needle);
+	needlelen = (int ) ft_strlen(needle);
 	if (needlelen == 0)
 		return ((char *)haystack);
 	i = 0;
-	while (haystack[i] && i <= ((int )(len - needlelen)))
+	while (i < (((int ) len) - needlelen) && haystack[i])
 	{
 		if (ft_strncmp(haystack + i, needle, needlelen) == 0)
 			return ((char *)haystack + i);
