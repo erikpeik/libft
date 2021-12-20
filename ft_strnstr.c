209b009512aeb2af94fb8b8510356d4f90699e73
Erik Mende <emende@student.hive.fi>
2021-12-20 13:38:48 +0200

    Casting needlelen to size_t

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: emende <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/17 22:21:03 by emende            #+#    #+#             */
-/*   Updated: 2021/12/14 18:44:54 by emende           ###   ########.fr       */
+/*   Updated: 2021/12/20 13:34:55 by emende           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,14 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 	int	needlelen;
 
 	needlelen = (int ) ft_strlen(needle);
+	if (len == 0)
+		return (NULL);
 	if (needlelen == 0)
 		return ((char *)haystack);
 	i = 0;
-	while (len != 0 && haystack[i] && (i <= (((int ) len) - needlelen)))
+	while (len != 0 && haystack[i] && i <= ((int) len - needlelen))
 	{
-		if (ft_strncmp(haystack + i, needle, needlelen) == 0)
+		if (ft_strncmp(haystack + i, needle, (size_t) needlelen) == 0)
 			return ((char *)haystack + i);
 		i++;
 	}
