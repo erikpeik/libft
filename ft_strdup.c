73e6237c7ea15e746a52d8de6497f94b45565c09
Erik Mende <emende@student.hive.fi>
2021-12-27 15:10:21 +0200

    strdup uses memcpy now

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: emende <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/11 13:19:06 by emende            #+#    #+#             */
-/*   Updated: 2021/12/09 19:20:31 by emende           ###   ########.fr       */
+/*   Updated: 2021/12/27 14:08:58 by emende           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,13 @@
 char	*ft_strdup(const char *s1)
 {
 	char	*copy;
-	size_t	i;
+	size_t	len;
 
-	copy = (char *)malloc(sizeof(char) * (ft_strlen(s1) + 1));
+	len = ft_strlen(s1);
+	copy = (char *)malloc(sizeof(char) * (len + 1));
 	if (!copy)
 		return (NULL);
-	i = 0;
-	while (s1[i])
-	{
-		copy[i] = s1[i];
-		i++;
-	}
-	copy[i] = '\0';
+	ft_memcpy(copy, s1, len);
+	copy[len] = '\0';
 	return (copy);
 }
