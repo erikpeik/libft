6305652e829c4ba758368551e2ef49bfded52b61
Erik Mende <emende@student.hive.fi>
2021-12-27 15:09:35 +0200

    Memset goes now long and int bytes at once.

--- ft_memset.c
+++ ft_memset.c
@@ -6,13 +6,13 @@
 /*   By: emende & altikka                           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/14 18:53:13 by emende            #+#    #+#             */
-/*   Updated: 2021/12/22 19:31:06 by emende           ###   ########.fr       */
+/*   Updated: 2021/12/27 13:49:14 by emende           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static void	*ft_og(void *b, int c, size_t len)
+static void	*ft_memset_one(void *b, int c, size_t len)
 {
 	size_t	i;
 
@@ -25,25 +25,25 @@ static void	*ft_og(void *b, int c, size_t len)
 void	*ft_memset(void *b, int c, size_t len)
 {
 	unsigned char	arr[sizeof(long)];
-	void			*ptr;
+	unsigned long	*ptr_l;
+	unsigned int	*ptr_i;
 
 	if (len < sizeof(long) + sizeof(int))
-		return (ft_og(b, c, len));
-	ptr = b;
-	ft_og((void *) arr, c, sizeof(long));
+		return (ft_memset_one(b, c, len));
+	ptr_l = b;
+	ft_memset_one((void *) arr, c, sizeof(long));
 	while (len >= sizeof(long))
 	{
-		*(long *) ptr = *(long *) arr;
-		ptr += sizeof(long);
+		*(long *) ptr_l++ = *(long *) arr;
 		len -= sizeof(long);
 	}
+	ptr_i = (unsigned int *) ptr_l;
 	if (len >= sizeof(int))
 	{
-		*(int *) ptr = *(int *) arr;
-		ptr += sizeof(int);
+		*(int *) ptr_i++ = *(int *) arr;
 		len -= sizeof(int);
 	}
-	while (len-- > 0)
-		*(unsigned char *) ptr++ = (unsigned char ) c;
+	if (len > 0)
+		ft_memset_one((void *) ptr_i, c, len);
 	return (b);
 }
