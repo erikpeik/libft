ed1ca3de3501a01b2eb49c3a626469197b32471d
Erik Mende <emende@student.hive.fi>
2022-01-13 15:32:47 +0200

    if oone is null strdup the other one

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: emende <emende@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/03 23:54:04 by emende            #+#    #+#             */
-/*   Updated: 2021/12/27 20:17:43 by emende           ###   ########.fr       */
+/*   Updated: 2021/12/28 14:50:04 by emende           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,11 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	size_t	s1_len;
 	size_t	s2_len;
 
-	if (!s1 || !s2)
+	if (!s1 && s2)
+		return (ft_strdup(s2));
+	if (s1 && !s2)
+		return (ft_strdup(s1));
+	if (!s1 && !s2)
 		return (NULL);
 	s1_len = ft_strlen(s1);
 	s2_len = ft_strlen(s2);
